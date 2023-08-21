c42cc05303d1d115923dbe22e7ce134e29aad66c
VICTORIA MATTHEWS <olayidecodes@gmail.com>
2023-08-21 15:57:07 +0100

    added active status for users

---
+++ app/components/ActiveStatus.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import useActiveChannel from './../hooks/UseActiveChannel';
+
+const ActiveStatus = () => {
+  useActiveChannel();
+
+  return null;
+}
+ 
+export default ActiveStatus;
--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,12 +2,17 @@
 
 import { User } from "@prisma/client";
 import Image from 'next/image';
+import useActiveList from "../hooks/useActiveList";
 
 interface AvatarProps {
     user?: User;
 }
 
 const Avatar: React.FC<AvatarProps> = ({user}) => {
+
+  const { members } = useActiveList()
+  const isActive = members.indexOf(user?.email!) !== -1
+
   return (
     <div className="relative">
       <div className="
@@ -26,24 +31,26 @@ const Avatar: React.FC<AvatarProps> = ({user}) => {
         fill
         />
       </div>
-      <span
-        className='
-          absolute
-          block
-          rounded-full
-          bg-green-500
-          ring-2
-          ring-white
-          top-0
-          right-0
-          h-2
-          w-2
-          md:h-3
-          md:w-3
-        '
-      >
-
-      </span>
+      {isActive && (
+        <span
+          className='
+            absolute
+            block
+            rounded-full
+            bg-green-500
+            ring-2
+            ring-white
+            top-0
+            right-0
+            h-2
+            w-2
+            md:h-3
+            md:w-3
+          '
+        >
+          
+        </span>
+      )}
     </div>
   )
 }
